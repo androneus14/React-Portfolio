c86eea83e9d8813c06bc1983912d7369f2811bbb
Andy Nguyen <andynguyen_3@hotmail.com>
2022-07-17 21:29:24 +1000

    Project js added

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,2 +1,21 @@
-import React, { useState } from 'react';
+import React from 'react';
+import projects from '../projects.json';
 
+export default function Project() {
+    return(
+        <div className='card'>
+            <img
+                className='card-image'
+                src={require(`../assets/images/${projects.image}`).default}
+                key={projects.title}
+            />
+            <div className='card-body'>
+                <h3 className='card-title'>
+                    <a href={projects.deployedLink}>{projects.title}</a>
+                </h3>
+                <p >{projects.description}</p>
+                <a href={projects.githubLink}>GitHub Repo</a>
+            </div>
+        </div>
+    )
+}
--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,2 +1,10 @@
-import React, { useState } from 'react';
+import React from 'react';
 
+export default function Resume() {
+    return (
+        <section className='resume-section'>
+            <h1 className='text-center'>Resume</h1>
+            <a href={require('../assets/resume/My-Resume.pdf').default}></a>
+        </section>
+    )
+}
