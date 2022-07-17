beea5f07bc80952b69267c21bcb3437eb59add43
Andy Nguyen <andynguyen_3@hotmail.com>
2022-07-17 22:14:20 +1000

    app and index added. testing

--- src/App.js
+++ src/App.js
@@ -1,2 +1,36 @@
 import React, { useState } from 'react';
 
+import Header from './components/Header';
+import Navigation from './components/Navigation';
+import Footer from './components/Footer';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Portfolio from './pages/Portfolio';
+import Resume from './pages/Resume';
+
+export default function App() {
+    const [section] = useState([ 'About', 'Portfolio', 'Contact', 'Resume' ])
+    const [currentSection, setCurrentSection] = useState(section[0]);
+
+    return(
+        <div>
+            <Header>
+                <Navigation>
+                    sections={section}
+                    currentSection={currentSection}
+                    setCurrentSection={setCurrentSection}
+                </Navigation>
+            </Header>
+
+            <body>
+                {(currentSection === 'About') ? <About />
+                : (currentSection === 'Portfolio') ? <Portfolio />
+                : (currentSection === 'Contact') ? <Contact />
+                : (currentSection === 'Resume') ? <Resume />
+                : <About />}
+            </body>
+
+            <Footer></Footer>
+        </div>
+    )
+}
