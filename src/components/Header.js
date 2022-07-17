d1c7d6c506e65a70cbfa7937a8de4933bf368667
Andy Nguyen <andynguyen_3@hotmail.com>
2022-07-17 21:53:02 +1000

    Header Nav added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,2 +1,14 @@
-import React, { useState } from 'react';
+import React from 'react';
+import Navigation from './Navigation';
+
+export default function Header({ currentSection, setCurrentSection }) {
+    return(
+        <header className='navbar'>
+            <h1>Andy Nguyen</h1>
+            <Navigation 
+                currentSection={currentSection} setCurrentSection={setCurrentSection}>
+            </Navigation>
+        </header>
+    )
+}
 
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,2 +1,22 @@
-import React, { useState } from 'react';
+import React from 'react';
 
+export default function Navigation({ currentSection, setCurrentSection }) {
+    const navigationLinks = ['About', 'Portfolio', 'Contact', 'Resume']; 
+    return(
+        <nav>
+            <ul className='nav-section'>
+                {navigationLinks.map(navigationLink => {
+                    <li
+                        key={navigationLink}
+                        onClick={() => setCurrentSection(navigationLink)}
+                        className={
+                            currentSection === navigationLink ? 'nav-link active nav-item': 'nav-link nav-item'
+                        }
+                    >
+                    {navigationLink}
+                    </li>
+                })}
+            </ul>
+        </nav>
+    )
+}
